645b0612f96535372244380ad0cfb064e8f22a72
wav-blue <wav0120@gmail.com>
2023-09-19 23:34:21 +0900

    08_정답

--- baekjoon 100제/08_좋은 수 구하기.py
+++ baekjoon 100제/08_좋은 수 구하기.py
@@ -1,17 +1,8 @@
 '''
-오답 사유: 음수가 들어오는 경우 고려 안함
+서로 다른 두 수의 합으로 표현되는 수가 있다면 좋은 수이다.
+좋은 수의 개수를 구하는 문제
 
-오류 케이스1 :
-4
--5 -1 1 2
-
-=> -1, 2를 더해서 1이 되는 경우 발견 X
-
-오류 케이스2 : 
-3
--1 1 2
-
-=> 2번째 인덱스부터 find하기 때문에 1을 발견 X
+- 투 포인터
 '''
 
 def answer_problem():
@@ -19,18 +10,23 @@ def answer_problem():
     nums = list(map(int, input().split(' ')))
     nums.sort()
     
-    find = 2
     answer = 0
     
-    while find < N:
+    for find in range(N):
         target = nums[find]
-        p1, p2 = 0, find-1
-        while p1 != p2:
+        p1, p2 = 0, N-1
+        while p1 < p2:
             now = nums[p1] + nums[p2]
-            #print(p1, nums[p1], p2, nums[p2], now, target)
+
             if now == target:
-                answer += 1
-                p1 = p2
+                if p1 != find and p2 != find:
+                    answer += 1
+                    break
+                else:
+                    if p1 == find:
+                        p1 += 1
+                    elif p2 == find:
+                        p2 -= 1
             elif now > target:
                 p2 -= 1
             else:
