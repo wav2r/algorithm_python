49359a9d1cc342dc67f7e24c6758872c434c1db2
wav-blue <wav0120@gmail.com>
2023-09-22 22:35:42 +0900

    add stack problem

---
+++ baekjoon 100제/11_스택으로_수열만들기.py
@@ -0,0 +1,33 @@
+# 1874 : Stack
+
+def answer_problem():
+    N = int(input())
+    
+    stack = [0]
+    tmp = [i+1 for i in range(N)]
+    tmp.sort(reverse=True)
+
+    answer = []
+    
+    for _ in range(N):
+        find = int(input())
+        
+        while stack[-1] < find:
+            stack.append(tmp.pop())
+            answer.append('+')
+
+        if stack[-1] == find:
+            stack.pop()
+            answer.append('-')
+        else:
+            return False
+    
+    return answer
+
+answer = answer_problem()
+
+if answer:
+    for ch in answer:
+        print(ch)
+else:
+    print('NO')
