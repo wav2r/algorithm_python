52a3365efb1d1be5e07b4b4ab5114561ca590fda
박수진 <wav0120@gmail.com>
2023-11-09 22:43:21 +0900

    sloved 수 정렬하기3:계수 정렬

--- baekjoon 100제/22_수_정렬하기3.py
+++ baekjoon 100제/22_수_정렬하기3.py
@@ -1,27 +1,17 @@
 #기수 정렬 문제
 #기수 정렬의 알고리즘을 그대로 구현한 코드 : 메모리 초과
-def now(num, d):
-    return num % d // (d//10)
+
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-numbers = []
-for _ in range(N):
-    numbers.append(int(input()))
 
-max_value = max(numbers) * 10 + 1
-div = 10
+count = [0] * 10001
 
-while max_value > div:
-    temp = [list() for _ in range(10)]
+for i in range(N):
+    count[int(input())] += 1
 
-    for number in numbers:
-        temp[now(number, div)].append(number)
-    
-    numbers = []
-
-    # merge temp
-    for arr in temp:
-        numbers += arr
-    div = div * 10
-
-print(numbers)
+for i in range(10001):
+    if count[i] != 0:
+        for _ in range(count[i]):
+            print(i)
