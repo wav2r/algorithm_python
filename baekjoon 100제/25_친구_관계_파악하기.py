8483a3846e8aed44b2689645ce9bdb4d69744546
박수진 <wav0120@gmail.com>
2023-11-12 15:57:11 +0900

    Solved 친구 관계 파악하기:DFS:exit 사용

--- baekjoon 100제/25_친구_관계_파악하기.py
+++ baekjoon 100제/25_친구_관계_파악하기.py
@@ -1,5 +1,6 @@
 # DFS - 재귀 방식
-# 1500ms
+# exit() 사용
+# 700ms
 
 import sys
 sys.setrecursionlimit(10000)
@@ -10,36 +11,31 @@ N, M = list(map(int, input().split()))
 visited = [False] * (N + 1)
 
 # input
-edge_dict = [[] for _ in range(N)]
+edges = [[] for _ in range(N)]
 
 for i in range(M):
-    a, b = list(map(int, input().split()))
-    edge_dict[a].append(b)
-    edge_dict[b].append(a)
+    a, b = map(int, input().split())
+    edges[a].append(b)
+    edges[b].append(a)
 
 # dfs function
 def dfs(now, count):
     global answer
-    
+
     if count == 4:
         answer = 1
-        return
-
-    for next in edge_dict[now]:
-        if visited[next]:
-            continue
-        visited[next] = True
-        dfs(next, count + 1)
-        visited[next] = False
+        print(1)
+        exit()
+    
+    visited[now] = True
+    for next in edges[now]:
+        if not visited[next]:
+            dfs(next, count + 1)
+    visited[now] = False
 
 # main
 answer = 0
 for start in range(N):
-    visited[start] = True
     dfs(start, 0)
-    visited[start] = False
 
-    if answer == 1:
-        break 
-
-print(answer)
+print(0)
